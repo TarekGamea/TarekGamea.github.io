afbc913c1f7e335dc6d98f9123f68844c516dc91
TarekGamea <tarekgamea2009@gmail.com>
2025-06-12 15:34:47 -0700

    Add files via upload

---
+++ portfolio/scripts/app.js
@@ -0,0 +1,4 @@
+const upButton = document.createElement('img');
+upButton.src = 'Up_Arrow_Symbol-removebg-preview.png'
+const downButton = document.createElement('img');
+downButton.src = 'Down_Arrow_Symbol-removebg-preview.png'
